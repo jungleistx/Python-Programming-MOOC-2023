ae210944c2c4726da517e209eebaa9e033e50c6b
Roope Vuorenlehto <rvuorenl@home.fi>
2023-06-28 12:28:56 +0300

    add part03, 01 exercises

---
+++ part03/01/power.py
@@ -0,0 +1,8 @@
+# Write your solution here
+
+limit = int(input("Upper limit: "))
+
+x = 1
+while x <= limit:
+    print(x)
+    x *= 2
---
+++ part03/01/power_n.py
@@ -0,0 +1,9 @@
+# Write your solution here
+
+limit = int(input("Upper limit: "))
+base = int(input("Base: "))
+
+x = 1
+while x <= limit:
+    print(x)
+    x *= base
