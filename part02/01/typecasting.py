51e14a3e948a5dad80ca2631855683512da37d45
Roope Vuorenlehto <rvuorenl@home.fi>
2023-06-28 12:15:31 +0300

    add part02, 01 exercises

---
+++ part02/01/typecasting.py
@@ -0,0 +1,8 @@
+# Write your solution here
+
+number = float(input("Please type in a number: "))
+
+int_part = int(number)
+
+print(f"Integer part: {int_part}")
+print(f"Decimal part: {number - float(int_part)}")
