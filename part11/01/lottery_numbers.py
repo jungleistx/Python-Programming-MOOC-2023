a125a1198f67270646bf1ed759dd1d8588bb9a3a
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2023-07-27 02:40:54 +0300

    add part11,01 lottery_numbers

---
+++ part11/01/lottery_numbers.py
@@ -0,0 +1,22 @@
+# WRITE YOUR SOLUTION HERE:
+
+class LotteryNumbers:
+	def __init__(self, week_nr:int, numbers:list):
+		self.week_nr = week_nr
+		self.__numbers = numbers
+
+	def number_of_hits(self, numbers:list):
+		return len([number for number in numbers if number in self.__numbers])
+
+	def hits_in_place(self, numbers:list):
+		return [number if number in self.__numbers else -1 for number in numbers]
+
+
+if __name__ == "__main__":
+	week5 = LotteryNumbers(5, [1,2,3,4,5,6,7])
+	my_numbers = [1,4,7,11,13,19,24]
+	print(week5.number_of_hits(my_numbers))
+
+	week8 = LotteryNumbers(8, [1,2,3,10,20,30,33])
+	my_numbers = [1,4,7,10,11,20,30]
+	print(week8.hits_in_place(my_numbers))
