39a6d9dc4381e43e2f9b7ce741a7476e830f7301
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2023-08-02 17:35:30 +0300

    add part12,01 ratings

---
+++ part12/01/ratings.py
@@ -0,0 +1,15 @@
+# TEE RATKAISUSI TÄHÄN:
+
+def sort_by_ratings(items:list) -> list:
+	def get_ratings(item:dict) -> float:
+		return item['rating']
+
+	return sorted(items, key=get_ratings, reverse=True)
+
+
+if __name__ == "__main__":
+	shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+	print("Rating according to IMDB")
+	for show in sort_by_ratings(shows):
+		print(f"{show['name']}  {show['rating']}")
