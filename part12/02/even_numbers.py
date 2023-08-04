1331eb969cb907e92c7b951bbe84c1c02bdfa0d9
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2023-08-04 12:11:03 +0300

    add part12,02 even_numbers

---
+++ part12/02/even_numbers.py
@@ -0,0 +1,18 @@
+# Write your solution here
+
+def even_numbers(beginning:int, maximum:int):
+	for i in range(beginning, maximum + 1):
+		if i % 2 == 0:
+			yield i
+
+
+if __name__ == "__main__":
+	numbers = even_numbers(2, 10)
+	for number in numbers:
+		print(number)
+
+	print()
+
+	numbers = even_numbers(11, 21)
+	for number in numbers:
+		print(number)
