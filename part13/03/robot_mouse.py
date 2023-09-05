3a69fbfd19fca2d46c4f46653cd8cfd5c39bf5d3
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2023-09-05 16:30:13 +0300

    mod robot_mouse, appear only when mouse_motion. Also remove clock, not needed

--- part13/03/robot_mouse.py
+++ part13/03/robot_mouse.py
@@ -12,10 +12,6 @@ robot = pygame.image.load("robot.png")
 
 half_robot_height = robot.get_height() // 2
 half_robot_width = robot.get_width() // 2
-x = 0
-y = 0
-
-clock = pygame.time.Clock()
 
 while True:
 	for event in pygame.event.get():
@@ -23,11 +19,9 @@ while True:
 			x = event.pos[0] - half_robot_width
 			y = event.pos[1] - half_robot_height
 
+			window.fill(BLACK)
+			window.blit(robot, (x,y))
+			pygame.display.flip()
+
 		if event.type == pygame.QUIT:
 			exit()
-
-	window.fill(BLACK)
-	window.blit(robot, (x,y))
-	pygame.display.flip()
-
-	clock.tick(60)
