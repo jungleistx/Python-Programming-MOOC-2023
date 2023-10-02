aae17e2e242f4824073cee97115a2af58880c737
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2023-10-02 16:29:33 +0300

    Revert "rename 02-01"
    This reverts commit 09af8673973d39e2fbd4b463f46a788412148a9a.

--- part02/01_programming_terminology/typecasting.py
+++
@@ -1,8 +0,0 @@
-# Write your solution here
-
-number = float(input("Please type in a number: "))
-
-int_part = int(number)
-
-print(f"Integer part: {int_part}")
-print(f"Decimal part: {number - float(int_part)}")
