217a121da04d52792528bc0d6c3525b060ae9a23
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2023-10-02 16:29:37 +0300

    Revert "rename 01-05"
    This reverts commit 9e5a9c82d7ae4c6354ce91b48297433a42fc3131.

--- part01/05_conditional_statements/absolute.py
+++
@@ -1,7 +0,0 @@
-# Write your solution here
-number = int(input("Please type in a number: "))
-
-if number < 0:
-    print(f"The absolute value of this number is {number * -1}")
-else:
-    print(f"The absolute value of this number is {number}")
--- part01/05_conditional_statements/temperatures.py
+++
@@ -1,9 +0,0 @@
-# Write your solution here
-
-temp = int(input("Please type in a temperature (F): "))
-celsius = (temp - 32) / 1.8
-
-print(f"{temp} degrees Fahrenheit equals {celsius} degrees Celsius")
-
-if celsius < 0:
-    print("Brr! It's cold in here!")
