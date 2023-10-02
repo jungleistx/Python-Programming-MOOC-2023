83870b9fc282894613651936847027fae0af870c
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2023-10-02 16:29:41 +0300

    Revert "rename 01-02"
    This reverts commit d7f821e569ab9b56c4dbb57dd18a9a52053c73bb.

--- part01/02_information_from_the_user/name_address.py
+++
@@ -1,8 +0,0 @@
-# Write your solution here
-first_name = input("Given name: ")
-last_name = input("Family name: ")
-address = input("Street address: ")
-city = input("City and postal code: ")
-print(first_name + " " + last_name)
-print(address)
-print(city)
--- part01/02_information_from_the_user/twice.py
+++
@@ -1,4 +0,0 @@
-# Write your solution here
-input_name = input("What is your name? ")
-print(input_name)
-print(input_name)
